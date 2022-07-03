7f62d7c39cbf503626ad246d2b651bd55d74dc26
Anup <anuppoudel60@gmail.com>
2022-07-03 13:30:48 +1000

    backend setup done

---
+++ config/db.js
@@ -0,0 +1,19 @@
+import mongoose from "mongoose";
+
+const mongoClient = async () => {
+  const conStr =
+    process.env.NODE_ENV === "production"
+      ? process.env.MONGO_CLIENT
+      : process.env.MONGO_URL;
+
+  try {
+    await mongoose
+      .connect(conStr)
+      .then((con) => console.log(`MongoDb connected : ${con.connection.host}`))
+      .catch((err) => console.log(err));
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+export default mongoClient;
