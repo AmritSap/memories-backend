2f25d6c849afc44cc087a023fd4695713ba69862
Anup <anuppoudel60@gmail.com>
2022-08-02 19:10:13 +1000

    CRUD post functionality completed

--- routes/post.js
+++ routes/post.js
@@ -63,4 +63,83 @@ router.get("/all", async (req, res) => {
     });
   }
 });
+
+// DELETE POST
+router.delete("/:id", isAuthenticated, async (req, res) => {
+  try {
+    const post = await Post.findById(req.params.id);
+
+    if (!post) {
+      return res.status(404).json({
+        success: false,
+        message: "Post not found",
+      });
+    }
+
+    if (post.owner.toString() !== req.user._id.toString()) {
+      return res.status(401).json({
+        success: false,
+        message: "You can only delete your post",
+      });
+    }
+
+    // await cloudinary.v2.uploader.destroy(post.image.public_id);
+
+    await post.remove();
+
+    const user = await User.findById(req.user._id);
+
+    const index = user.posts.indexOf(req.params.id);
+
+    user.posts.splice(index, 1);
+
+    await user.save();
+
+    res.status(200).json({
+      success: true,
+      message: "Post deleted",
+    });
+  } catch (error) {
+    res.status(500).json({
+      status: "error",
+      message: error.message,
+    });
+  }
+});
+
+router.put("/:id", isAuthenticated, async (req, res) => {
+  try {
+    const post = await Post.findById(req.params.id);
+
+    if (!post) {
+      return res.status(404).json({
+        success: false,
+        message: "Post not found",
+      });
+    }
+
+    if (post.owner.toString() !== req.user._id.toString()) {
+      return res.status(401).json({
+        success: false,
+        message: "Unauthorized",
+      });
+    }
+
+    post.title = req.body.title;
+    post.desc = req.body.desc;
+
+    await post.save();
+
+    return res.status(200).json({
+      success: true,
+      message: "Your post has been updated",
+    });
+  } catch (error) {
+    res.status(500).json({
+      status: "error",
+      message: error.message,
+    });
+  }
+});
+
 export default router;
